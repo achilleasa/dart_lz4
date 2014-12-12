f040313bb8d0462867da00f783b4d5ad75f80c69
Achilleas Anagnostopoulos <achilleas@taxibeat.com>
2014-12-12 08:22:37 +0200

    Updated test-runner script

--- test/run.sh
+++ test/run.sh
@@ -3,7 +3,8 @@
 # Make sure cmake is installed and use it to build
 # our native extension before running the tests
 sudo apt-get install cmake
-cmake --build .
+cmake .
+make
 
 # Some tests rely on the native extension performing type checks on the
 # input parameters. To test this we need to run our tests in non-checked mode
